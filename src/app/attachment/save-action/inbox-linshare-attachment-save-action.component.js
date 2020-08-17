1765961862d8d4219838cfec5c07c509cd21d522
Huy TA QUOC <htquoc@linagora.com>
2020-08-17 13:43:25 +0700

    https://github.com/OpenPaaS-Suite/esn-frontend-inbox/issues/60: Replaced templateUrl with template in 'inboxLinshareAttachmentSaveAction' component

--- src/app/attachment/save-action/inbox-linshare-attachment-save-action.component.js
+++ src/app/attachment/save-action/inbox-linshare-attachment-save-action.component.js
@@ -2,7 +2,7 @@
 
 angular.module('linagora.esn.unifiedinbox.linshare')
   .component('inboxLinshareAttachmentSaveAction', {
-    templateUrl: require('./inbox-linshare-attachment-save-action.pug'),
+    template: require('./inbox-linshare-attachment-save-action.pug'),
     controller: 'inboxLinshareAttachmentSaveActionController',
     bindings: {
       attachment: '<'
